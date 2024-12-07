47b2fad463e6a50cf42a03017c514153763d8727
Kolli Greeshma Chowdary <116976957+Greeshmakc7@users.noreply.github.com>
2024-12-07 08:53:59 +0530

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,57 @@
+package com.klef.jfsd.exam.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Order {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private String productName;
+    private Integer quantity;
+    private Date orderDate;
+    private String customerName;
+
+    // Getters and setters
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+}
